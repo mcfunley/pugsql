97799d20280bf29c29cf85c453261a1986d9e174
Dan McKinley <dmckinley@rsglab.com>
2019-05-23 19:17:26 -0700

    removing dumb tests

--- tests/test_pugsql.py
+++ tests/test_pugsql.py
@@ -2,10 +2,6 @@ from pugsql import __version__, create_module, compiler
 from unittest import TestCase
 
 
-def test_version():
-    assert __version__ == '0.1.0'
-
-
 def test_create_module():
     compiler.modules.clear()
     assert create_module('tests/sql').sqlpath == 'tests/sql'
