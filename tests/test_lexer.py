e5466dbd67134cc8d5d44f1e55662958cb58bc2d
Dan McKinley <dmckinley@rsglab.com>
2019-05-25 18:25:59 -0700

    some lexer tests

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -8,3 +8,19 @@ class LexerTest(TestCase):
             ('C', '-- :name username_for_id :1'),
             ('Q', 'select username from users where user_id = :user_id'),
         ], lexer.lex(open('tests/sql/basic.sql', 'r').read()))
+
+    def test_leading_comment_whitespace(self):
+        self.assertEqual([
+            ('C', '-- :name username_for_id :1'),
+            ('Q', 'select username from users where user_id = :user_id'),
+        ], lexer.lex(
+            '   -- :name username_for_id :1\n'
+            'select username from users where user_id = :user_id'))
+
+    def test_whitespace(self):
+        self.assertEqual([
+            ('C', '-- :name username_for_id :1'),
+            ('Q', 'select username from users where user_id = :user_id'),
+        ], lexer.lex(
+            ' -- :name username_for_id :1  \n'
+            ' select username from users where user_id = :user_id  '))
