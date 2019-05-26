90f299149e0101ba42d6b6b6b469d3da1fe0f60a
Dan McKinley <dmckinley@rsglab.com>
2019-05-26 13:42:56 -0700

    docstrings for exceptions

--- pugsql/exceptions.py
+++ pugsql/exceptions.py
@@ -1,8 +1,22 @@
+"""
+Exception types raised by PugSQL. PugSQL will also raise built-in exceptions
+when they're appropriate.
+"""
+
+__pdoc__ = {}
+
 class ParserError(ValueError):
     """
     Exception raised when syntax errors are encountered parsing PugSQL files.
     """
+    token = None
+
     def __init__(self, message, token):
+        """
+        Creates a new ParserError given a message and the token indicating the
+        position of the error. Builds a user-facing error message indicating
+        the file, line, and column of the error.
+        """
         super(ParserError, self).__init__(
             'Error in %s:%s:%s - %s' % (
                 token.context.sqlfile,
@@ -10,3 +24,7 @@ class ParserError(ValueError):
                 token.context.col,
                 message))
         self.token = token
+
+__pdoc__['ParserError.token'] = (
+    'The `pugsql.lexer.Token` indicating the position of the error '
+    'encountered during parsing.')
