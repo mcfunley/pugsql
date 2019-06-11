08e7474a81a661dddc9d02b17d5e1ca1a6a76807
Abe Winter <abe@none>
2019-06-10 23:11:07 -0400

    don't initialize, use getattr() to check

--- pugsql/compiler.py
+++ pugsql/compiler.py
@@ -37,7 +37,6 @@ class Module(object):
         self._sessionmaker = None
 
         self._locals = threading.local()
-        self._locals.session = None
 
         for sqlfile in glob(os.path.join(self.sqlpath, '*sql')):
             with open(sqlfile, 'r') as f:
@@ -80,7 +79,7 @@ class Module(object):
         For more info, see here:
         https://docs.sqlalchemy.org/en/13/orm/session_transaction.html
         """
-        if not hasattr(self._locals, 'session') or not self._locals.session:
+        if not getattr(self._locals, 'session', None):
             if not self._sessionmaker:
                 raise NoConnectionError()
 
