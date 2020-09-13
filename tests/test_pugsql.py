87538e60b8c36a21143d467134174048c9b912cb
Dan McKinley <mcfunley@gmail.com>
2020-09-12 22:40:50 -0700

    fixes tests for python 3.5

--- tests/test_pugsql.py
+++ tests/test_pugsql.py
@@ -193,8 +193,8 @@ class PugsqlTest(TestCase):
         pugsql.module('tests/sql/extra-dashes')
 
     def test_nesting_transactions_rollback(self):
-        id: int = None
-        id2: int = None
+        id = None
+        id2 = None
         with self.fixtures.transaction() as tr1:
             id = self.fixtures.insert_user(username='little_bug')
             with self.fixtures.transaction() as tr2:
@@ -204,7 +204,7 @@ class PugsqlTest(TestCase):
                 id2 = self.fixtures.insert_user(username='little_bug2')
                 tr2.commit()
             tr1.rollback()
-        # TODO
+
         self.assertNotEqual(
             {'username': 'little_bug', 'user_id': id},
             self.fixtures.user_for_id(user_id=id))
